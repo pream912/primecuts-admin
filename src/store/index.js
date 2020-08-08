8482f9622d7ae6515eadea412e8329c15bc95939
pream912 <preamkumar912@gmail.com>
2020-08-08 16:46:16 +0530

    unknown updates

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,31 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import firebase from 'firebase/app'
+import 'firebase/database'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    circles: []
   },
   mutations: {
+    ADD_CIRCLE (state, payload) {
+      state.circles.push(payload)
+    }
   },
   actions: {
+    addCircle ({commit}, payload) {
+      firebase.database().ref('circles').push(payload)
+      .then((data) => {
+        commit('ADD_CIRCLE', data.val())
+      })
+    }
+  },
+  getters: {
+    loadedCircles(state) {
+      return state.circles
+    }
   },
   modules: {
   }
--- src/views/Circles.vue
+++ src/views/Circles.vue
@@ -5,14 +5,21 @@
                 <v-btn @click="new_circle_dialog = true">New circle</v-btn>
             </v-col>
             <v-col cols="12">
-                <v-select :items="items" label="Select circle"></v-select>
+                <v-select :items="loadedCircles" label="Select circle"></v-select>
             </v-col>
         </v-row>
         <v-row>
             <v-dialog v-model="new_circle_dialog" persistent max-width="300px">
                 <v-card>
                     <v-card-text>
-                        <v-text-field label="Circle name" v-model="new_circle_name"></v-text-field>
+                        <v-row>
+                            <v-col cols="12">
+                                <v-text-field label="Circle name" v-model="new_circle_name"></v-text-field>
+                            </v-col>
+                            <v-col cols="12">
+                                
+                            </v-col>
+                        </v-row>
                     </v-card-text>
                     <v-card-actions>
                         <v-btn>Create</v-btn>
@@ -27,9 +34,14 @@
 <script>
 export default {
     data: () => ({
-        items: [],
         new_circle_dialog: false,
         new_circle_name: ''
-    })
+    }),
+
+    computed: {
+        loadedCircles () {
+            return this.$store.getters.loadedCircles
+        }
+    }
 }
 </script>
